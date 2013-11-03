b500f8228286025d605a1f0d20948f1062b44ab2
Lu Huiguo <luhuiguo@gmail.com>
2013-11-03 21:00:31 +0800

    .

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -247,7 +247,11 @@ angular.module('app.controllers', [])
       var symbol = $routeParams.symbol;
       var ticker = quoteService.getTicker(symbol);
 
+
+      console.log('11111111111');
       var requestTrend = function() {
+
+         console.log('1111111112222222211');
         var stompClient = quoteService.stompClient;
         stompClient.send("/app/trend/" + symbol, {}, '');
       };
@@ -339,7 +343,7 @@ angular.module('app.controllers', [])
         });
 
 
-        requestTrend();
+        //requestTrend();
 
       }
 
